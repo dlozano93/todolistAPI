541e58179324e90bc36b5eb94f0d65f2d4428e11
David Lozano <dloznao@gmail.com>
2020-02-22 02:09:50 +0000

    second commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,9 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Card() {
 	const [todo, setTodo] = useState();
 	const [list, setList] = useState([]);
+
+	function getToDo() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/dlozano93")
+			.then(resp => resp.json())
+			.then(data => {
+				setList(data);
+				console.log("getToDo", data);
+			});
+	}
+
+	useEffect(() => {
+		getToDo();
+	}, []);
+
+	function saveToDo(listToSave) {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/dlozano93", {
+			method: "PUT",
+			body: JSON.stringify(listToSave),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				console.log("saveToDo", data);
+				getToDo();
+			});
+	}
+
 	return (
 		<div className="card w-50 mx-auto">
 			<div className="card-body text-center">
@@ -13,7 +42,12 @@ export function Card() {
 						onChange={e => setTodo(e.target.value)}
 						onKeyPress={e => {
 							if (e.key === "Enter") {
-								setList(list.concat(todo));
+								saveToDo(
+									list.concat({
+										label: todo,
+										done: false
+									})
+								);
 								setTodo("");
 							}
 						}}
@@ -24,16 +58,31 @@ export function Card() {
 				</div>
 				{list.map((item, index) => {
 					return (
-						<div className="alert alert-primary" key={index}>
+						<div
+							className={
+								item.done
+									? "alert alert-primary"
+									: "alert alert-secondary"
+							}
+							key={index}>
 							<div className="row">
-								<div className="col-6">{item}</div>
+								<div className="col-6">{item.label}</div>
 								<div className="col text-center">
 									<button
 										type="button"
 										className="btn btn-success"
 										onClick={() =>
-											setList(
-												list.filter(e => e !== item)
+											saveToDo(
+												list.map((e, i) => {
+													if (index === i) {
+														return {
+															label: e.label,
+															done: true
+														};
+													} else {
+														return e;
+													}
+												})
 											)
 										}>
 										Done
